783b9dd5e75657af2a7c5bdecf076ff1313c92f2
rkdgml3577 <101076374+rkdgml3577@users.noreply.github.com>
2022-05-12 16:15:37 +0900

    백엔드 디비구축

---
+++ back-end/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const port = 3000
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect('mongodb+srv://rkdgml:choi0730!A@laon.joias.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    })   
