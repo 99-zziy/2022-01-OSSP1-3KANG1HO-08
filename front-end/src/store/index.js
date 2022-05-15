fad5bf180553cf54cce14bacca50c8453e8a214d
99-zziy <wldud8229@gmail.com>
2022-05-16 00:48:11 +0900

    Feat: 리덕스 기본 설정

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { user } from "../store/user";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
