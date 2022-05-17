e38033628c984cecafa8d27d176925afa6bc4392
wonho <wonho1401@naver.com>
2022-05-17 16:06:35 +0900

    Feat: 유효성 검증 및 dispatch 구현

--- front-end/src/pages/RegisterPage/Register.js
+++ front-end/src/pages/RegisterPage/Register.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { PrimaryColor } from "../../assets/color/color";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../store/user";
 
 const Container = styled.div`
   display: flex;
@@ -52,7 +56,7 @@ const Button = styled.button`
   margin: auto 0;
 `;
 
-const Form = styled.div`
+const Form = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -60,40 +64,131 @@ const Form = styled.div`
   margin: 0;
 `;
 
-function Register() {
+function Register(props) {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <Container>
-        <Title>회원 가입</Title>
-        <Form>
-          <InputContainer>
-            아이디
-            <p />
-            <Input type="text" name="id" placeholder="아이디" />
-          </InputContainer>
-          <InputContainer>
-            비밀번호
-            <p />
-            <Input
-              type="password"
-              name="password"
-              placeholder="비밀번호"
-            ></Input>
-          </InputContainer>
-          <InputContainer>
-            비밀번호 확인
-            <p />
-            <Input
-              type="password"
-              name="password"
-              placeholder="비밀번호 확인"
-            ></Input>
-          </InputContainer>
-          <p />
-          <Button>회원가입</Button>
-        </Form>
-      </Container>
-    </div>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email("이메일이 올바르지 않습니다.")
+          .required("이메일은 필수 항목입니다."),
+        password: Yup.string()
+          .min(6, "최소 6자 이상 입력해주세요.")
+          .required("비밀번호는 필수 항목입니다."),
+        confirmPassword: Yup.string()
+          .oneOf([Yup.ref("password"), null], "비밀번호와 일치해야합니다.")
+          .required("비밀번호 확인은 필수 항목입니다."),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          let dataToSumbit = {
+            email: values.email,
+            password: values.password,
+          };
+          dispatch(registerUser(dataToSumbit)).then((response) => {
+            if (response.payloda.success) {
+              props.history.push("/login");
+            } else {
+              alert(response.payload.err.errmsg);
+            }
+          });
+          setSubmitting(false);
+        }, 500);
+      }}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleSubmit,
+          handleBlur,
+        } = props;
+        return (
+          <div>
+            <Container>
+              <Title>회원 가입</Title>
+              <Form onSubmit={handleSubmit}>
+                <InputContainer>
+                  이메일
+                  <p />
+                  <Input
+                    required
+                    type="text"
+                    id="email"
+                    placeholder="이메일"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.email && touched.email
+                        ? "text-input error"
+                        : "text-input"
+                    }
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+                </InputContainer>
+                <InputContainer>
+                  비밀번호
+                  <p />
+                  <Input
+                    required
+                    type="password"
+                    id="password"
+                    placeholder="비밀번호"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.password && touched.password
+                        ? "text-input error"
+                        : "text-input"
+                    }
+                  />
+                  {errors.password && touched.password && (
+                    <div className="input-feedback">{errors.password}</div>
+                  )}
+                </InputContainer>
+                <InputContainer>
+                  비밀번호 확인
+                  <p />
+                  <Input
+                    required
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="비밀번호 확인"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.confirmPassword && touched.confirmPassword
+                        ? "text-input error"
+                        : "text-input"
+                    }
+                  />
+                  {errors.confirmPassword && touched.confirmPassword && (
+                    <div className="input-feedback">
+                      {errors.confirmPassword}
+                    </div>
+                  )}
+                </InputContainer>
+                <p />
+                <Button onClick={handleSubmit} disabled={isSubmitting}>
+                  회원가입
+                </Button>
+              </Form>
+            </Container>
+          </div>
+        );
+      }}
+    </Formik>
   );
 }
 
