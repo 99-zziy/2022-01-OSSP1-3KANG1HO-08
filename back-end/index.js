a190ad3cf8200dadb5d65a2da741e5d6912c18cc
rkdgml3577 <101076374+rkdgml3577@users.noreply.github.com>
2022-05-24 21:45:11 +0900

    Edit : 코드 수정

--- back-end/index.js
+++ back-end/index.js
@@ -1,149 +1,145 @@
+//서버의 시작점
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { Feed } = require("./models/Feed");
+const router = express.Router();
 
-    //서버의 시작점
-    const express = require("express");
-    const app = express();
-    const port = 5000;
-    const bodyParser = require("body-parser");
-    const cookieParser = require("cookie-parser");
-    const { User } = require("./models/User");
-    const { auth } = require("./middleware/auth");
-    
-    const methodOverride = require('method-override');
-    const { Feed } = require("./models/Feed");
-    const router = express.Router();
-    const mongoose = require("mongoose");  
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose
+  .connect('mongodb+srv://rkdgml:choi0730!A@laon.joias.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 
+app.post("/users/signup", (req, res) => {
+  // 회원 가입할 때 넣는 정보들을 user에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    
-    app.use(cookieParser());
-    mongoose
-        .connect('mongodb+srv://rkdgml:choi0730!A@laon.joias.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-        .then(() => console.log('MongoDB Connected...'))
-        .catch(err => console.log(err))
-    
-    
-    app.post("/users/signup", (req, res) => {
-      // 회원 가입할 때 넣는 정보들을 user에서 가져오면
-      // 그것들을 데이터베이스에 넣어준다.
-    
-      const user = new User(req.body);
-      user.save((err, userInfo) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-          success: true,
-        });
-      });
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
     });
-    
-    app.post("/users/login", (req, res) => {
-      // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
-      User.findOne({ email: req.body.email }, (err, user) => {
-       
-        if (!user) {
-          return res.json({
-            loginSuccess: false,
-            message: "제공된 이메일에 해당하는 유저가 없습니다.",
-          });
-        }
-        // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 일치하는지 확인한다.
-        user.comparePassword(req.body.password, (err, isMatched) => {
-          if (!isMatched)
-            return res.json({
-              loginSuccess: false,
-              message: "비밀번호가 틀렸습니다.",
-            });
-        });
-        // 비밀번호까지 일치하다면 토큰을 생성한다.
-        user.generateToken((err, user) => {
-          if (err) return res.status(400).send(err);
-    
-          // 토큰을 쿠키에 저장
-          res
-            .cookie("x_auth", user.token)
-            .status(200)
-            .json({ loginSuccess: true, userId: user._id });
-        });
-      });
-    });
-    
-    app.get("/api/users/auth", auth, (req, res) => {
-      // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true라는 것.
-      res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-      });
-    });
-    
-    app.get("/users/logout", auth, (req, res) => {
-      
-      User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({ success: true });
-      });
-    });
-    
+  });
+});
 
-    app.post("/feeds",(req, res) => {
 
-      Feed.create(req.body, (err, post) => {
-        if(err) return res.json(err);
-        
-        return res.status(200).json({
-          success:true,
-        });
-        
-      });
+app.post("/users/login", async (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return res.json({
+      loginSuccess: false,
+      message: "이메일에 해당하는 유저가 없습니다.",
     });
 
-
-    app.get("/feeds/:id/edit", (req,res)=>{
-      Feed.findOne({_id:req.params.id},(err, Feed)=>{
-        if(err) return res.json(err);
-        res.render('index',{Feed:Feed});
-      })
-    })
-
-
-    app.put("/feeds/:id", (req,res) =>{
-      req.body.updateAt = Date.now();
-      Feed.findOneAndUpdate({_id:req.params.id}, req.body, (err, Feed)=>{
-        if(err) return res.json(err);
-       
-        return res.status(200).json({
-          success:true,
-        });
-      })
-    })
-
-    app.get('/feeds/:id', (req,res) => {
-      Feed.findOne({_id:req.params.id}, (err,feeds)=>{
-        if(err) return res.json(err);
-        return res.status(200).send({feeds : feeds})
-    
+  // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 일치하는지 확인한다.
+  return user.comparePassword(req.body.password, (err, isMatched) => {
+    if (!isMatched)
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다.",
       });
-    });
-    
-    app.delete('/feeds/:id', (req,res)=>{
-      Feed.deleteOne({_id:req.params.id}, (req,res)=>{
-        if(err) return res.json(err);
-      });
-    });
 
-    app.get('/feeds', (req,res)=>{
-      Feed.find({})
-      .sort('-createdAt')
-      .exec((err,feeds)=>{
-        if(err) return res.json(err);
-        return res.status(200).send({ feeds: feeds });
-      });
+    // 비밀번호까지 일치하다면 토큰을 생성한다.
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+
+      res// 토큰을 쿠키에 저장
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id });
     });
+  });
+});
 
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-    // ======= 기준으로 두개 나뉘어져있는데 저기서 둘중에 더 맞는거 선택해서 아닌거는 지우면됨 
+app.post("/feeds", (req, res) => {
+
+  Feed.create(req.body, (err, feeds) => {
+    if (err) return res.json(err);
+
+    return res.status(200).send({ feeds: feeds })
+
+  });
+});
+
+
+app.get("/feeds/:id/edit", (req, res) => {
+  Feed.findOne({ _id: req.params.id }, (err, feed) => {
+    if (err) return res.json(err);
+    res.render('index', { feeds: feeds });
+  })
+})
+
+
+app.put("/feeds/:id", (req, res) => {
+  req.body.updateAt = Date.now();
+  Feed.findOneAndUpdate({ _id: req.params.id }, req.body, (err, feeds) => {
+    if (err) return res.json(err);
+
+    return res.status(200).send({ feeds: feeds })
+  })
+})
+
+app.get('/feeds/:id', (req, res) => {
+  Feed.findOne({ _id: req.params.id }, (err, feeds) => {
+    if (err) return res.json(err);
+    return res.status(200).send({ feeds: feeds })
+
+  });
+});
+
+app.delete('/feeds/:id', (req, res) => {
+  Feed.deleteOne({ _id: req.params.id }, (req, res) => {
+    if (err) return res.json(err);
+    return res.status(200).send({ feeds: feeds })
+  });
+});
+
+app.get('/feeds', (req, res) => {
+  Feed.find({})
+    .sort('-createdAt')
+    .exec((err, feeds) => {
+      if (err) return res.json(err);
+      return res.status(200).send({ feeds: feeds });
+    });
+});
+
+app.get("/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true라는 것.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+  });
+});
+
+app.get("/users/logout", auth, (req, res) => {
+  console.log(req);
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- back-end/models/Feed.js
+++ back-end/models/Feed.js
@@ -6,7 +6,7 @@ const feedSchema = mongoose.Schema(
   {
     userFrom: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: User,
     },
     // ObjectId를 타입으로 하면 ref:"User"를 통해 User.js 에서 유저 정보를 가져올 수 있음.
     
@@ -19,12 +19,17 @@ const feedSchema = mongoose.Schema(
     },
     tag: {
         type: Array,
+    },
+    user: {
+      type:Object,
     }
 
   },
   { timestamps: true }
 );
 console.log(feedSchema)
+
+
 const Feed = mongoose.model("Feed", feedSchema);
 
 module.exports = { Feed };
