bf300769bd128f38562d5dcfb477d48beb27c867
99-zziy <wldud8229@gmail.com>
2022-05-25 16:57:50 +0900

    Feat: Feed 리스트 가져와서 홈에서 보여주기

--- front-end/src/components/FeedPreview.js
+++ front-end/src/components/FeedPreview.js
@@ -59,23 +59,19 @@ const Like = styled.div`
   margin: 1rem;
 `;
 
-function FeedPreview() {
+function FeedPreview({ title, content, date, likeCount }) {
   // api 통신 코드 추가하면 실제 데이터 넣을 것, 지금은 더미 데이터
   return (
     <FeedPreviewCard>
       <FeedPreviewMain>
         <TextContainer>
-          <Title>{"SSR CSR 차이점"}</Title>
-          <Content>
-            {
-              "새로운 페이지를 요청 할때 마다 전체 페이지가 다시 렌더링 된다. 화면에서 바뀌지 않아도 되는 부분도 계속해서 다시 렌더링되는 단점이 있기 때문에 곧 서버 부하 등의 문제를 일으킬 수 있다.또한 전체가 다시 렌더링 될때 화면의 깜빡이게 되므로 사용자에게 불편함을 줄 수 있다."
-            }
-          </Content>
-          <Date>{"3일전"}</Date>
+          <Title>{title}</Title>
+          <Content>{content}</Content>
+          <Date>{date}</Date>
         </TextContainer>
       </FeedPreviewMain>
       <FeedPreviewFooter>
-        <Like>{"♥ 35"}</Like>
+        <Like>{`♥ ${likeCount}`}</Like>
       </FeedPreviewFooter>
     </FeedPreviewCard>
   );
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { getFeedList } from "../api/feedApi";
 import FeedPreview from "../components/FeedPreview";
 
 const FeedPreviewContainer = styled.div`
@@ -10,14 +11,29 @@ const FeedPreviewContainer = styled.div`
 `;
 
 function Home() {
+  const [feedList, setFeedList] = useState(null);
+
+  useEffect(() => {
+    getFeedList().then((res) => {
+      console.log(res.feeds);
+      setFeedList(res.feeds);
+    });
+  }, []);
+
   return (
     <FeedPreviewContainer>
-      <FeedPreview></FeedPreview>
-      <FeedPreview></FeedPreview>
-      <FeedPreview></FeedPreview>
-      <FeedPreview></FeedPreview>
-      <FeedPreview></FeedPreview>
-      <FeedPreview></FeedPreview>
+      {feedList &&
+        feedList.map((feed, index) => {
+          return (
+            <FeedPreview
+              key={index}
+              title={feed.title}
+              content={feed.contents}
+              date={feed.createdAt}
+              likeCount={6}
+            ></FeedPreview>
+          );
+        })}
     </FeedPreviewContainer>
   );
 }
